6a547a7a122eda5c95c421af4e493fe2fce5f733
Archit1992 <gajjararchit00004@gmail.com>
2016-06-12 12:19:58 +0530

    adding ex2 calling functions...

---
+++ C++/ex2.cpp
@@ -0,0 +1,22 @@
+/**************************************************************
+* Author    :		 Archit, Gajjar
+* Date      :        12, June 2016
+* Time      :        11:52 (IST)
+* IDE  		:        Notepad++
+* Version   :		 cpp 5.1.0 
+**************************************************************/
+#include<iostream>
+using namespace std;
+
+void f(){
+	cout << "hello world";
+}
+int avg(int a, int b){
+	return (a+b)/2; // return avg value.
+}
+int main(){
+	
+	f(); // call to f() function.
+	int avgVal = avg(2,4); // calling avg() and return avg value.
+	cout << " avg value is ::" << avgVal; // print avg value.
+}
