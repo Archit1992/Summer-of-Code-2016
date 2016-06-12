cdbb888677cadd3ab56062c6c19676786c854292
Archit1992 <gajjararchit00004@gmail.com>
2016-06-12 12:19:31 +0530

    adding ex1 hello world.

---
+++ C++/ex1.cpp
@@ -0,0 +1,17 @@
+/**************************************************************
+* Author    : Archit, Gajjar
+* Date      : 11, June 2016
+* Time      : 10:15 (IST)
+* IDE       : Notepad++
+* Version   : cpp 5.1.0 
+**************************************************************/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout << "hello world "; // print hello world.
+	return 0;
+}
