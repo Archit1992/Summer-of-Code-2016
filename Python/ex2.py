6763909ae6f2c95583c7b1fc0a8299c49ea4fe59
Archit1992 <gajjararchit00004@gmail.com>
2016-06-14 21:52:26 +0530

    adding ex2.py math functions...

---
+++ Python/ex2.py
@@ -0,0 +1,31 @@
+#**************************************************************
+# Author    : Archit, Gajjar
+# Date      : 14, June 2016
+# Time      : 21:45 (IST)
+# IDE       : Notepad++
+# Version   : 2.7.11 
+#*************************************************************/
+
+# math functions.. 
+print "Now I am gonna do some math functions..."
+print "total cookies",25+30/5
+print "roosters ", 100 - 25 * 3 % 4 # give first priority to the Multiplication then Modulo and after that Minus operator. 
+print "value of 75 % 4 is =", 75 % 4
+
+print 3+2+1-5+4 % 2-1 /4+6  # first prioroty given to the 2-1 and 4+6 later on Divide operator then Modulo.
+print " 2-1 / 4+6 is ", 2-1/4+6
+ 
+print "Is it true that 5+2 < 3+1 ?"
+print 5+2 < 3+1 
+
+print "what is 5 + 2 ?", 5+2 
+print "what is 5 - 2 is",5-2
+print "what is 5 % 5 is", 5%5
+print "what is 5 % 2 is ", 5%2
+print "what is 5 % 7 is ", 5%7 
+
+print "what about some logical operators.."
+print "Is 5 greater than 2 ?", 5>2 
+print "Is 5 greater than or equal to 2?", 5>=2 
+print "Is 5 less than or equal to -2 ? ", 5 <= -2 
+print "Is 5 equals to 5 ?", 5==5  
